b387d2058e55db0bf14394d7dfcf61bead49e12e
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2026-07-07 11:11:45 +0530

    Update Intersection of Two Linked Lists.py

--- DSA-FreshStart/LinkedList/Intersection of Two Linked Lists.py
+++ DSA-FreshStart/LinkedList/Intersection of Two Linked Lists.py
@@ -52,40 +52,63 @@ class Solution:
 
         #Optimized Approach
         #length of the first LL
-        n1=0
-        currA=headA
-        while(currA!=None):
-            n1+=1
-            currA=currA.next
+        # n1=0
+        # currA=headA
+        # while(currA!=None):
+        #     n1+=1
+        #     currA=currA.next
 
-        #length of the second LL
-        n2=0
-        currB=headB
-        while(currB!=None):
-            n2+=1
-            currB=currB.next
+        # #length of the second LL
+        # n2=0
+        # currB=headB
+        # while(currB!=None):
+        #     n2+=1
+        #     currB=currB.next
 
-        #now check the difference and confirm the longest LL, and as per that move the pointer of the largest LL (both at the same position)
-        dif=0
-        currA=headA
-        currB=headB
-        if(n1>n2):
-            dif=n1-n2
-            while(dif>0 and currA!=None):
-                currA=currA.next
-                dif-=1
-        else:
-            dif=n2-n1
-            while(dif>0 and currB!=None):
-                currB=currB.next
-                dif-=1
+        # #now check the difference and confirm the longest LL, and as per that move the pointer of the largest LL (both at the same position)
+        # dif=0
+        # currA=headA
+        # currB=headB
+        # if(n1>n2):
+        #     dif=n1-n2
+        #     while(dif>0 and currA!=None):
+        #         currA=currA.next
+        #         dif-=1
+        # else:
+        #     dif=n2-n1
+        #     while(dif>0 and currB!=None):
+        #         currB=currB.next
+        #         dif-=1
         
-        #now currA and currB will be at the same position, traverse and check the same node (intersection point)
-        while(currA!=None and currB!=None):
-            if(currA==currB):
-                return currA
-            currA=currA.next
-            currB=currB.next
+        # #now currA and currB will be at the same position, traverse and check the same node (intersection point)
+        # while(currA!=None and currB!=None):
+        #     if(currA==currB):
+        #         return currA
+        #     currA=currA.next
+        #     currB=currB.next
 
-        #no intersection point is there
-        return None
+        # #no intersection point is there
+        # return None
+
+
+        #More Optimized Approach
+        #if in both any one head is None, then there is no intersection
+        if(headA==None or headB==None):
+            return None
+
+        temp1=headA
+        temp2=headB
+        while(temp1!=temp2):
+            temp1=temp1.next
+            temp2=temp2.next
+
+            if(temp1==temp2):
+                return temp1
+
+            if(temp1==None):
+                temp1=headB
+            if(temp2==None):
+                temp2=headA
+
+        #comes out of the loop when both the node matches
+        return temp1
